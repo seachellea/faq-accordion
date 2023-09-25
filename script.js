d8229452d09f9210c466a9368d3aa5e27c5306e1
Rochelle Andes <143592080+seachellea@users.noreply.github.com>
2023-09-26 00:05:46 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+document.querySelectorAll('.accordion__question').forEach(item => {
+    item.addEventListener('click', (event) => {
+        console.log('click!');
+        let accCollapse = item.nextElementSibling;
+
+        if(!item.classList.contains('collapsing')) {
+            // Open accordion item
+            if(!item.classList.contains('open')) {
+                console.log('toggle accordion collapsing');
+                
+                accCollapse.style.display = 'block';
+                let accHeight = accCollapse.clientHeight;
+                console.log(accHeight);
+
+                setTimeout(() => {
+                    accCollapse.style.height = accHeight + 'px';
+                    accCollapse.style.display = '';
+                }, 1);
+                
+                accCollapse.classList = 'accordion__collapse collapsing';
+                setTimeout(() => {
+                    console.log('open accordion content');
+                    accCollapse.classList = 'accordion__collapse collapse open';
+                }, 300);
+            }
+
+            // Close accordion item
+            else {
+                accCollapse.classList = 'accordion__collapse collapsing';
+
+                setTimeout (() => {
+                    accCollapse.style.height = '0px';
+                }, 1);
+
+                
+                setTimeout(() => {
+                    console.log('close accordion content');
+                    accCollapse.classList = 'accordion__collapse collapse';
+                    accCollapse.style.height = '';
+                }, 300);
+            }
+
+            item.classList.toggle('open');
+        }
+        
+    })
+})
+
